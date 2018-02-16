79ac31a220ffe36588ba36e4c52353affbfaa92b
David Heiman <dheiman@broadinstitute.org>
2018-02-16 15:57:12 -0500

    test configuration updated to copy user config files to tmp user dir.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,30 @@
 import pytest
 import tempfile
 import shutil
+import os
 
 def pytest_configure(config):
     """
     Setup empty temporary user directory for purposes of testing, and make it
     so os.path.expanduser() returns it instead of the user's home directory
+    
+    Copies relevant config files into the temporary directory
     """
     import os.path
     userdir = tempfile.mkdtemp()
+    
+    orig_userdir = os.path.expanduser('~')
+    fissconfig = os.path.join(orig_userdir, '.fissconfig')
+    gsutil_cfg = os.path.join(orig_userdir, '.gsutil')
+    docker_cfg = os.path.join(orig_userdir, '.docker')
+    
+    if os.path.isfile(fissconfig):
+        shutil.copy2(fissconfig, os.path.join(userdir, '.fissconfig'))
+    if os.path.isdir(gsutil_cfg):
+        shutil.copytree(gsutil_cfg, os.path.join(userdir, '.gsutil'))
+    if os.path.isdir(docker_cfg):
+        shutil.copytree(docker_cfg, os.path.join(userdir, '.docker'))
+    
     def expanduser(path):
         if not path.startswith('~'):
             return path
@@ -17,6 +33,7 @@ def pytest_configure(config):
         if idx < 0:
             idx = len(path)
         return userdir + path[idx:]
+    
     os.path.expanduser=expanduser
     config._userdir = userdir
 
