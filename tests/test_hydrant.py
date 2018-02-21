2a6a210eee88dabf38973f20740caeb129a8907a
David Heiman <dheiman@broadinstitute.org>
2018-02-21 14:04:40 -0500

    Test all the help calls

--- tests/test_hydrant.py
+++ tests/test_hydrant.py
@@ -6,3 +6,13 @@ def test_main():
     with pytest.raises(SystemExit) as excinfo:
         hydrant.main()
     assert str(excinfo.value) == '0'
+
+@pytest.fixture(scope='module',
+                params=['init', 'validate', 'build', 'push', 'test', 'install'])
+def hydrant_func(request):
+    return request.param
+
+def test_help(hydrant_func):
+    with pytest.raises(SystemExit) as excinfo:
+        hydrant.main([hydrant_func, '-h'])
+    assert str(excinfo.value) == '0'
